74e1c18cf7051b72d7c569c7c0707e7cc12c8b87
Jeremy Garrell <garrellj101@gmail.com>
2024-04-20 22:40:53 -0400

    Add solution for led roulette exercise

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,47 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let pattern = [
+        (0, 0), (0, 1), (0, 2), (0, 3),
+        (0, 4), (1, 4), (2, 4), (3, 4),
+        (4, 4), (4, 3), (4, 2), (4, 1),
+        (4, 0), (3, 0), (2, 0), (1, 0)
+    ];
+
+    loop {
+        for coordinates in pattern {
+            let board_lights = board_with_led_lit_at(coordinates.0, coordinates.1);
+            display.show(&mut timer, board_lights, 30);
+            timer.delay_ms(10_u32);
+        }
+    }
+}
+
+fn board_with_led_lit_at(row: usize, column: usize) -> [[u8; 5]; 5] {
+    let mut board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    board[row][column] = 1;
+
+    return board
 }
